7613faf7e5a5d356424ebb3e8c73423025c0f3bc
admin <admin@qq.com>
2018-11-26 19:42:14 +0800

    第一次推送

---
+++ src/main/java/com/itheima/test.java
@@ -0,0 +1,15 @@
+package com.itheima;
+
+public class test {
+
+
+    public static void main(String[] args) {
+        System.out.println("hallo");
+        System.out.println("hallo1");
+        System.out.println("hallo2");
+        System.out.println("hallo3");
+        System.out.println("hallo4");
+        System.out.println("hallo5");
+
+    }
+}
